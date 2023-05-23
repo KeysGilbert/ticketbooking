d447e8d99279dea0e1a668958aeeaf464d841e27
KeysGilbert <keysgilbert@gmail.com>
2023-05-23 14:09:07 -0400

    implement model for customer

---
+++ ticketbooking/src/main/java/com/example/ticketbooking/models/Customer.java
@@ -0,0 +1,54 @@
+package com.example.ticketbooking.models;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
+public class Customer {
+
+    @NotNull(message = "An id is required.")
+    int customerId;
+
+    @NotNull(message = "A first name is required.")
+    @NotBlank(message = "First name cannot be blank")
+    String firstName;
+
+    @NotNull(message = "A last name is required.")
+    @NotBlank(message = "Last name cannot be blank.")
+    String lastName;
+
+    @NotNull(message = "Email is required.")
+    @NotBlank(message = "Email cannot be blank.")
+    String email;
+
+    public int getCustomerId() {
+        return customerId;
+    }
+
+    public void setCustomerId(int customerId) {
+        this.customerId = customerId;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
